72a267b9a3d6e631d141b61d75d1107348aaa0af
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2021-03-23 16:39:34 -0400

    Challenge 1.mjs: without beforeOrAfter

---
+++ Challenge 1.mjs
@@ -0,0 +1,44 @@
+import formatBenchmark from './utils/sort by fastest formatBenchmark().mjs'
+
+import Benchmark from 'benchmark'
+import assert from 'assert'
+
+var suite = new Benchmark.Suite;
+
+const arr = Array.from(Array(100000).keys())
+const reverse = true
+// const reverse = false
+
+// console.log(sum)
+// process.exit(0)
+
+// add tests
+// -5000 or 5000
+suite
+  .add('repeat yourself', function () {
+    let sum = 0
+    if (reverse) {
+      for (let i = arr.length - 1; i > -1; i--) {
+        sum = arr[i] - sum
+      }
+    } else {
+      for (let i = 0, len = arr.length; i < len; i++) {
+        sum = arr[i] - sum
+      }
+    }
+    assert(sum === -50000)
+  })
+  .add('magic', function () {
+    //your stuff here
+    assert(sum === -50000)
+  })
+
+  // add listeners
+  .on('complete', function () {
+    formatBenchmark(this)
+  })
+  // run async
+  .run({
+    'async': true
+  });
+
